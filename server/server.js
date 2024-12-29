8d1b8510dfc0a0e21b8e3045a53c4059fc82ca3a
Harsha <podetiharshavardhan@gmail.com>
2024-12-29 20:53:01 +0530

    helo

--- server/server.js
+++ server/server.js
@@ -12,13 +12,14 @@ dotenv.config();
 async function initializeDB() {
     try {
         const db = await mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
+            host: process.env.MYSQL_ADDON_HOST,
+            user: process.env.MYSQL_ADDON_USER,
+            password: process.env.MYSQL_ADDON_PASSWORD,
+            database: process.env.MYSQL_ADDON_DB,
             waitForConnections: process.env.DB_WAIT_FOR_CONNECTIONS === 'true',
             connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT || 10, 10),
             queueLimit: parseInt(process.env.DB_QUEUE_LIMIT || 0, 10),
+            port: process.env.MYSQL_ADDON_PORT,
         });
         console.log('Database connected successfully');
         return db;
